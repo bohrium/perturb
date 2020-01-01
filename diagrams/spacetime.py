eea27aced86eb8346a7adcf4b8d442df4d30129f
Samuel <samuel@umich.edu>
2020-01-01 14:48:16 -0500

    spacetime better examples

--- diagrams/spacetime.py
+++ diagrams/spacetime.py
@@ -12,11 +12,15 @@ from PIL import Image, ImageDraw, ImageFont
 fnt = ImageFont.truetype('Pillow/Tests/fonts/DejaVuSans.ttf', 24)
 
 black = (0.0, 0.0, 0.0)
+
 red   = (0.8, 0.2, 0.2)
-green = (0.2, 0.8, 0.2)
-blue  = (0.2, 0.2, 0.8)
 gold  = (0.9, 0.7, 0.0)
-colors = [red, green, blue, gold, black]
+green = (0.2, 0.8, 0.2)
+cyan  = (0.0, 0.9, 0.7)
+blue  = (0.2, 0.2, 0.8)
+royal = (0.7, 0.0, 0.9)
+
+colors = [red, green, blue, gold, cyan, royal, black]
 
 def draw_disk_aa(img, row, col, rad=7, color=black):
     expanded_color = np.expand_dims(np.expand_dims(np.array(color), 0), 0)
@@ -30,8 +34,6 @@ def draw_disk_aa(img, row, col, rad=7, color=black):
 def draw_line_aa(img, row_s, col_s, row_e, col_e, color=black):
     expanded_color = np.expand_dims(np.expand_dims(np.array(color), 0), 0)
     rr, cc, val = line_aa(row_s, col_s, row_e, col_e)
-    #img[rr, cc, :] = np.expand_dims(val, 2)
-    #img[rr, cc, :] = img[rr, cc, :] * expanded_color
     img[rr, cc, :] = np.minimum(img[rr, cc, :], np.expand_dims(val, 2) * expanded_color)
 
 def draw_arc_aa(img, row_s, col_s, row_e, col_e, curve=1.0, color=black):
@@ -47,8 +49,8 @@ def draw_arc_aa(img, row_s, col_s, row_e, col_e, curve=1.0, color=black):
     for (r, c), (rr, cc) in zip(points, points[1:]):
         draw_line_aa(img, r, c, rr, cc, color)
 
-N = 8
-T = 8
+N = 5
+T = 15
 MARG = 30
 SPACE = 60
 height = 3*MARG + N*SPACE
@@ -72,10 +74,10 @@ def fill_boxes(img, nts):
         img[rr, cc, :] = 0.9
 
 def draw_nodes(img, nts):
-    ns = sorted(list(set(int(n) for n,t in nts if t < T-0.5)))
+    #ns = sorted(list(set(int(n) for n,t in nts)))
     for n, t in nts:
-        if not (t < T-0.5): continue
-        draw_disk_aa(img, get_r(n+0.5), get_c(t+0.5), color=colors[ns.index(int(n))])
+        #draw_disk_aa(img, get_r(n+0.5), get_c(t+0.5), color=colors[ns.index(int(n))])
+        draw_disk_aa(img, get_r(n+0.5), get_c(t+0.5), color=colors[int(n+0.5)])
 
 def draw_diagram(img, ntss):
     for nts in ntss:
@@ -84,27 +86,34 @@ def draw_diagram(img, ntss):
 
 def draw(filename): 
     img = np.ones((height, width, 3), dtype=np.float32)
-    fill_boxes(img, [(i%N, i%T) for i in range(max(N, T))])
+    fill_boxes(img, [(
+        #(i*(int(i//N) + 1)) % N,
+        i                  % N,
+        i%T
+    ) for i in range(max(N, T))])
     draw_grid(img)
 
-    # an order 5 diagram:
-    draw_diagram(img, [[(5, 0+0.2), (4, 2), (5, 4), (5.5    , 8    )],
-                       [(5, 0-0.2),                 (5.5    , 8    )],
-                       [            (6, 3), (5, 4),                 ]])
+    # an order 1 diagram:
+    draw_diagram(img, [[(3, 0    ),                 (5.0-0.2, 1.5  )]])
+    draw_diagram(img, [[(0, 1    ),                 (5.0-0.2, 2.5  )]])
 
     # an order 3 diagram (high degree):
-    draw_diagram(img, [[(7, 7+0.3),                 (7.5-0.5, 8    )],
-                       [(7, 7+0.0),                 (7.5-0.5, 8    )],
-                       [(7, 7-0.3),                 (7.5-0.5, 8    )]])
+    draw_diagram(img, [[(3, 3+0.3),                 (5.0-0.2, 4.5  )],
+                       [(3, 3+0.0),                 (5.0-0.2, 4.5  )],
+                       [(3, 3-0.3),                 (5.0-0.2, 4.5  )]])
 
-    # an order 1 diagram:
-    draw_diagram(img, [[(4, 4    ),                 (4.5    , 8    )]])
+    # an order 5 diagram:
+    draw_diagram(img, [[(1, 3+0.2), (0, 4), (2, 5), (5.0-0.2, 6.5  )],
+                       [(1, 3-0.2),                 (5.0-0.2, 6.5  )],
+                       [            (1, 4), (2, 5),                 ]])
 
     # translates of an order 2 diagram:
-    draw_diagram(img, [[(1, 5    ),                 (0.5    , 8    )], 
-                       [            (0, 6),         (0.5    , 8    )]])
-    draw_diagram(img, [[(1, 4    ), (2, 6),         (1.5    , 8    )]])
-    draw_diagram(img, [[(3, 0    ), (1, 3),         (2.5    , 8    )]])
+    draw_diagram(img, [[(1,  6   ),                 (5.0-0.2, 9.5  )], 
+                       [            (3, 8),         (5.0-0.2, 9.5  )]])
+    draw_diagram(img, [[(4, 9    ), (0,10),         (5.0-0.2,11.5  )]])
+    draw_diagram(img, [[(4,12    ),                 (5.0-0.2,13.5  )], 
+                       [            (3,12),         (5.0-0.2,13.5  )]])
+    draw_diagram(img, [[(1,11    ), (3,13),         (5.0-0.2,14.5  )]])
 
     plt.imsave(filename, img)
 
@@ -116,10 +125,10 @@ draw('spacetime.png')
 im = Image.open("spacetime.png")
 
 draw = ImageDraw.Draw(im)
-draw.text((get_c(0.18), get_r(8+0.7)), "Space Time for Pure GD and Pure SGD", font=fnt, fill=(0,0,0,255))
-for t in range(T):
+#draw.text((get_c(6.18), get_r(N+0.7)), "Space Time for Pure GD and Pure SGD", font=fnt, fill=(0,0,0,255))
+for t in range(0, T, 2):
     draw.text((get_c(t+0.1), get_r(0.0)), "t=%d"%t, font=fnt, fill=(0,0,0,255))
-for n in range(N):
+for n in range(0, N, 2):
     draw.text((get_c(-0.9), get_r(n+0.7)), "n=%d"%n, font=fnt, fill=(0,0,0,255))
 
 
