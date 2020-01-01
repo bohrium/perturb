cd3c828bea9c08714eb47300ff6bf545e3dfe249
Samuel <samuel@umich.edu>
2019-12-31 22:21:11 -0500

    gd and sgd spacetime

--- diagrams/spacetime.py
+++ diagrams/spacetime.py
@@ -1,12 +1,12 @@
 ''' author: samuel tenka
-    change: 2019-03-25 
+    change: 2019-12-31 
     create: 2019-03-25 
     descrp: render SGD diagrams   
 '''
 
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage.draw import circle, circle_perimeter_aa, line_aa
+from skimage.draw import circle, circle_perimeter_aa, line_aa, rectangle
 from PIL import Image, ImageDraw, ImageFont
 
 fnt = ImageFont.truetype('Pillow/Tests/fonts/DejaVuSans.ttf', 24)
@@ -40,7 +40,7 @@ def draw_arc_aa(img, row_s, col_s, row_e, col_e, curve=1.0, color=black):
     col_diff = (col_e-col_s)
 
     get_rr = lambda t: int(row_s + (row_e-row_s)*t - curve * (1.0-(2*t-1)**2)**0.5 * col_diff) 
-    get_cc = lambda t: int(col_s + (col_e-col_s)*t + curve * (1.0-(2*t-1)**2)**0.5 * row_diff)  
+    get_cc = lambda t: int(col_s + (col_e-col_s)*t)#+ curve * (1.0-(2*t-1)**2)**0.5 * row_diff)  
     times = np.arange(0.0, 1.0+1e-5, 1.0/int(length/3.0))
     times = ((np.abs(2*times-1) * np.sign(2*times-1))+1)/2
     points = [(get_rr(t), get_cc(t)) for t in times] 
@@ -66,6 +66,11 @@ def draw_edges(img, nts):
     for (n, t), (nn, tt) in zip(nts, nts[1:]):
         draw_arc_aa(img, get_r(n+0.5), get_c(t+0.5), get_r(nn+0.5), get_c(tt+0.5), curve=0.2)
 
+def fill_boxes(img, nts):
+    for (n, t) in nts:
+        rr, cc = rectangle((get_r(n), get_c(t)), (get_r(n+1), get_c(t+1)))
+        img[rr, cc, :] = 0.9
+
 def draw_nodes(img, nts):
     ns = sorted(list(set(int(n) for n,t in nts if t < T-0.5)))
     for n, t in nts:
@@ -79,6 +84,7 @@ def draw_diagram(img, ntss):
 
 def draw(filename): 
     img = np.ones((height, width, 3), dtype=np.float32)
+    fill_boxes(img, [(i%N, i%T) for i in range(max(N, T))])
     draw_grid(img)
 
     # an order 5 diagram:
@@ -95,10 +101,10 @@ def draw(filename):
     draw_diagram(img, [[(4, 4    ),                 (4.5    , 8    )]])
 
     # translates of an order 2 diagram:
-    draw_diagram(img, [[(1, 1    ), (0, 3),        (-0.5    , 8    )]])
-    draw_diagram(img, [[(1, 4    ), (1, 7),         (0.5    , 8    )]])
-    draw_diagram(img, [[(1, 0    ), (2, 4),         (1.5    , 8    )]])
-    draw_diagram(img, [[(3, 5    ), (3, 6),         (2.5    , 8    )]])
+    draw_diagram(img, [[(1, 5    ),                 (0.5    , 8    )], 
+                       [            (0, 6),         (0.5    , 8    )]])
+    draw_diagram(img, [[(1, 4    ), (2, 6),         (1.5    , 8    )]])
+    draw_diagram(img, [[(3, 0    ), (1, 3),         (2.5    , 8    )]])
 
     plt.imsave(filename, img)
 
@@ -110,10 +116,11 @@ draw('spacetime.png')
 im = Image.open("spacetime.png")
 
 draw = ImageDraw.Draw(im)
-draw.text((get_c(1.5), get_r(8+0.7)), "Space Time for Pure GD", font=fnt, fill=(0,0,0,255))
+draw.text((get_c(0.18), get_r(8+0.7)), "Space Time for Pure GD and Pure SGD", font=fnt, fill=(0,0,0,255))
 for t in range(T):
-    draw.text((get_c(t+0.1), get_r(0.0)), "T=%d"%t, font=fnt, fill=(0,0,0,255))
+    draw.text((get_c(t+0.1), get_r(0.0)), "t=%d"%t, font=fnt, fill=(0,0,0,255))
 for n in range(N):
-    draw.text((get_c(-0.9), get_r(n+0.7)), "N=%d"%n, font=fnt, fill=(0,0,0,255))
+    draw.text((get_c(-0.9), get_r(n+0.7)), "n=%d"%n, font=fnt, fill=(0,0,0,255))
+
 
 im.save('spacetime.png')
