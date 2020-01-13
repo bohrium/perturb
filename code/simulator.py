855d95c11da1fc5a9e4c9a297f33ddb613356b86
Samuel <samuel@umich.edu>
2020-01-13 14:14:50 -0500

    format

--- code/simulator.py
+++ code/simulator.py
@@ -59,9 +59,9 @@ def compute_losses(land, eta, T, N, I=1, idx=None, opts=opts, test_extra=300,
             nabla = land.nabla
             stalk = land.get_loss_stalk
 
-    #-------------------------------------------------------------------------#
-    #               0.1 define optimization updates                           #
-    #-------------------------------------------------------------------------#
+            #-----------------------------------------------------------------#
+            #       0.1 define optimization updates                           #
+            #-----------------------------------------------------------------#
 
             compute_gradients = {
                 'SGD':  lambda t:   nabla(stalk(D_train[(t%N):(t%N)+1]))  ,
@@ -78,9 +78,9 @@ def compute_losses(land, eta, T, N, I=1, idx=None, opts=opts, test_extra=300,
                 ),
             }[opt]
 
-    #-------------------------------------------------------------------------#
-    #               0.2 perform optimization loop                             #
-    #-------------------------------------------------------------------------#
+            #-----------------------------------------------------------------#
+            #       0.2 perform optimization loop                             #
+            #-----------------------------------------------------------------#
 
             land.switch_to(idx)
             for t in range(T):
@@ -88,9 +88,9 @@ def compute_losses(land, eta, T, N, I=1, idx=None, opts=opts, test_extra=300,
                     -eta * compute_update(compute_gradients(t)).detach()
                 )
 
-    #-------------------------------------------------------------------------#
-    #               0.3 compute losses and accuracies                         #
-    #-------------------------------------------------------------------------#
+            #-----------------------------------------------------------------#
+            #       0.3 compute losses and accuracies                         #
+            #-----------------------------------------------------------------#
 
             test_loss = land.get_loss_stalk(D_test).detach().numpy()
             test_acc = land.get_accuracy(D_test)
@@ -114,32 +114,32 @@ def compute_losses(land, eta, T, N, I=1, idx=None, opts=opts, test_extra=300,
 #           1. SET SIMULATION HYPERPARAMETER RANGES                           #
 #=============================================================================#
 
-    #-------------------------------------------------------------------------#
-    #               1.0 sanity check on quadratic landscape                   #
-    #-------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
+#                   1.0 sanity check on quadratic landscape                   #
+#-----------------------------------------------------------------------------#
 
-def test_on_quad_landscape():
+def test_on_quad_landscape(T=100):
     LC = Quadratic(dim=12)
     DIM = 8
     Q = Quadratic(dim=DIM)
     ol = OptimLog()
     for eta in tqdm.tqdm(np.arange(0.0005, 0.005, 0.001)):
-        for T in [100]:
-            ol.absorb(compute_losses(Q, eta=eta, T=T, N=T, I=int(100000.0/(T+1))))
+        ol.absorb(compute_losses(Q, eta=eta, T=T, N=T, I=int(100000.0/(T+1))))
     print(ol)
     with open('ol.data', 'w') as f:
         f.write(str(ol))
-    print(CC+'measured @G {:+.1f} @W - @G {:+.1f} @W \t expected @Y {:.1f} @W '.format(
+    print(CC+'measured @G {:+.1f} @D - @G {:+.1f} @D \t '+
+             'expected @Y {:.1f} @D '.format(
         mean - 1.96 * stdv/nb_samples**0.5,
         mean + 1.96 * stdv/nb_samples**0.5,
         DIM/2.0 + DIM/2.0 * (1-ETA)**(2*T)
     ))
 
-    #-------------------------------------------------------------------------#
-    #               1.1 lenet                                                 #
-    #-------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
+#                   1.1 lenet                                                 #
+#-----------------------------------------------------------------------------#
 
-def simulate_lenet(idxs, T, N, I=100, eta_step=0.025, eta_max=0.25,
+def simulate_lenet(idxs, T, N, I=100, eta_d=0.025, eta_max=0.25,
                    model=CifarLeNet, in_nm='saved-weights/cifar-lenet.npy',
                    out_nm=lambda idx:'ol-cifar-lenet-{:02d}.data'.format(idx)):
     '''
@@ -148,9 +148,12 @@ def simulate_lenet(idxs, T, N, I=100, eta_step=0.025, eta_max=0.25,
     LC.load_from(in_nm, nb_inits=6, seed=0)
     for idx in tqdm.tqdm(idxs):
         ol = OptimLog()
-        for eta in tqdm.tqdm(np.arange(eta_step, eta_max+eta_step/2, eta_step)):
+        for eta in tqdm.tqdm(np.arange(eta_d, eta_max+eta_d/2, eta_d)):
             for T in [T]:
-                ol.absorb(compute_losses(LC, eta=eta, T=T, N=T, I=I, idx=idx, opts=[('SGD', None)]))
+                ol.absorb(compute_losses(
+                    LC, eta=eta, T=T, N=T, I=I, idx=idx,
+                    opts=[('SGD', None)]
+                ))
 
         with open(out_nm(idx), 'w') as f:
             f.write(str(ol))
@@ -158,5 +161,8 @@ def simulate_lenet(idxs, T, N, I=100, eta_step=0.025, eta_max=0.25,
 if __name__=='__main__':
     import sys
     T = int(sys.argv[1])
-    simulate_lenet(range(6), T=T, N=T, I=int(50000/T), eta_step=0.025, eta_max=0.25,
-            out_nm=lambda idx:'ol-cifar-lenet-T{}-{:02d}.data'.format(T, idx))
+    simulate_lenet(
+        range(6), T=T, N=T, I=int(50000/T),
+        eta_step=0.025, eta_max=0.25,
+        out_nm=lambda idx:'ol-cifar-lenet-T{}-{:02d}.data'.format(T, idx)
+    )
